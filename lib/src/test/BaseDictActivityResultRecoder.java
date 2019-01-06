9d98a9a297481966c12c094d2891af7665be49a6
wangxuechao <djun100@qq.com>
2019-01-06 20:02:18 +0800

    init

---
+++ lib/src/test/BaseDictActivityResultRecoder.java
@@ -0,0 +1,21 @@
+package test;
+
+public abstract class BaseDictActivityResultRecoder {
+	
+	BaseDictActivityResultRecoder(){};
+	
+	public String getResAt(int pos) {return "";}
+	
+	public void renderContentAt(int pos) {}
+	
+	public int size() {return 0;}
+	
+	public void invalidate() {}
+	
+	public void invalidate(int adapter_idx) {}
+
+	public void shutUp() {}
+	
+	public int dictIdx=0;
+
+}
